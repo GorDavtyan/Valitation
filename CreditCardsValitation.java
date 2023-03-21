2f7f7bb36966178c6cc2236e72939efe03eeec02
GorDavtyan <87947216+GorDavtyan@users.noreply.github.com>
2023-03-21 18:27:44 +0400

    Update CreditCardsValitation.java

--- CreditCardsValitation.java
+++ CreditCardsValitation.java
@@ -63,7 +63,6 @@ public class CreditCardsValitation {
                 }
             } catch (Exception ex) {
                 System.out.println("This cannot be a card number");
-                return;
             }
         }
 
@@ -77,7 +76,6 @@ public class CreditCardsValitation {
                 }
             } catch (Exception ex) {
                 System.out.println("There is no such bank card");
-                return;
             }
         }
     }
